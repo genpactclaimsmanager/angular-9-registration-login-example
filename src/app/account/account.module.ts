c62a1feb38221da650c84b3e7c27fd20a885923e
BSHARSANA <b.s.harsana@gmail.com>
2020-08-27 12:58:49 +0530

    Update account.module.ts

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AccountRoutingModule } from './account-routing.module';
 import { LayoutComponent } from './layout.component';
@@ -10,6 +11,7 @@ import { RegisterComponent } from './register.component';
 @NgModule({
     imports: [
         CommonModule,
+        HttpClientModule,
         ReactiveFormsModule,
         AccountRoutingModule
     ],
